fdee7012c565fdc146a08ff8177cee1dbe3a6d2e
Manoj Nayak <manojvn2612@gmail.com>
2026-02-10 14:32:46 +0530

    capture images

---
+++ test.py
@@ -0,0 +1,38 @@
+import cv2
+import os
+
+os.makedirs("dataset", exist_ok=True)
+
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cap.set(cv2.CAP_PROP_ZOOM, 2.9)
+cap.set(cv2.CAP_PROP_FOCUS, 1159)
+cap.set(cv2.CAP_PROP_AUTO_WB, 0)
+#2783
+#2.9->1159
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)  # manual
+cap.set(cv2.CAP_PROP_EXPOSURE, -7)         # shorter exposure
+cap.set(cv2.CAP_PROP_GAIN, 5)              # compensate brightness
+
+
+i = 0
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    cv2.imshow("AFDM412", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('c'):
+        filename = f"dataset/img_{i:04d}.jpg"
+        cv2.imwrite(filename, frame)
+        print("Saved", filename)
+        i += 1
+    elif key == 27:  # ESC
+        break
+
+cap.release()
+cv2.destroyAllWindows()
